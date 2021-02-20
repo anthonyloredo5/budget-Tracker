16d183c3486a9618037494b4456d41bbc468e419
Anthony Loredo <anthonyloredo5@gmail.com>
2021-02-19 22:26:40 -0500

    added db offline functionality using indexdb

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = 'mongodb+srv://anthonyloredo5:anthonyloredo5123@cluster0.r7wzt.mongodb.net/budget?retryWrites=true&w=majority';
 const app = express();
 
 app.use(logger("dev"));
@@ -16,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
